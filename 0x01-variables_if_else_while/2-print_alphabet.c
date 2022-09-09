fc54b7b761a645bf0b38776004450a39c269c22c
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-09 02:52:46 -0700

    printing lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always a success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+
+	return (0);
+}
