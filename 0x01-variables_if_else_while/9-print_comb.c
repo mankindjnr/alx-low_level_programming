f89182023c194fc3e8602db9371a4eb38ec97e6c
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-09 03:54:05 -0700

    printing combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,16 @@ int main(void)
 	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+
+		if (i == 9)
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
