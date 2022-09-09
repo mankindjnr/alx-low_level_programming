bce80a9725bb003817ca191db045643b521d0c97
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-09 03:41:58 -0700

    print combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
