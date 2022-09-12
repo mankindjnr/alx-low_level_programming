d12494b9b3c065e22ecfdd401815cbc12e52cb0b
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-12 05:42:23 -0700

    absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+/**
+ *_abs - absolute value finder
+ *@int: finding the absolute of
+ *Return: Always 0
+ */
+int _abs(int r)
+{
+	return (abs(r));
+}
