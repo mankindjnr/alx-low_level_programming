6ac82af1f4089d69ef2c1d84b72fa02b23a19388
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-12 06:50:42 -0700

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "math.h"
+#include <unistd.h>
+/**
+ *print_to_98 - to 98
+ *@n: the start
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%i", i);
+
+			if ( i < 98)
+			{
+				printf(", ");
+			}
+			else
+			{
+				break;
+			}
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%i", i);
+
+			if (i == 98)
+			{
+				break;
+			}
+			else
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+	{
+		printf("%i\n", n);
+	}
+}
