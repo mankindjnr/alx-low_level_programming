2a88d678d130daeb30eb9c9d21e68e86342d0967
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-12 22:11:14 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,5 +9,22 @@
  */
 int print_last_digit(int r)
 {
-	return (r % 10);
+	int last;
+
+	last = r % 10;
+	if (last % 10 > 0)
+	{
+		_putchar(last + '0');
+	}
+	else if (last == 0)
+	{
+		last = 0;
+		_putchar(last + '0');
+	}
+	else if (last < 0)
+	{
+		last = last * -1;
+		_putchar(last + '0');
+	}
+	return (last);
 }
