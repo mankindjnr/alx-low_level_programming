1fa6af0f632378d16ddf2ba23ca352c45ca4d595
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-14 07:42:46 -0700

    infinite not happening

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
- *main -entry point
- *
- *Return: always zero
- */
+* main - causes an infinite loop
+* Return: 0
+*/
+
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
+        printf("Infinite loop incoming :(\n");
 
-	return (0);
+        i = 0;
+
+	/*while (i < 10)
+        {
+                putchar(i);
+        }
+	*/
+        printf("Infinite loop avoided! \\o/\n");
+        return (0);
 }
