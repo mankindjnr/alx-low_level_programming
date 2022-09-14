995c92e48883d3419ca0f78b5e302eb0e532c2d0
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-14 07:33:55 -0700

    test positive or negative

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main -entry point
+ *
+ *Return: always zero
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+creating test files in main.c's
