39bba4c15a058d3751f0c3fb411d5d6731d83681
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-15 01:35:48 -0700

    mulitply them

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ *mul - multiplying integers
+ *@a: integer 1
+ *@b: integer 2
+ *Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
