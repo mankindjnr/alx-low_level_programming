dab796cab4433bb65ff91d2f6cb4d2f78f8a383d
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-15 03:15:02 -0700

    digonally straight

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ *print_diagonal - diagonally straight
+ *@n: how long
+ */
+void print_diagonal(int n)
+{
+	int i, k;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (k = 0; k <= i; k++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
