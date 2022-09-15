cc6d52b23508978284d3583e55cd62a399e352cb
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-15 01:50:47 -0700

    not 2 or a 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ *print_most_numbers - not 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
