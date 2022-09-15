3fae249b9c0de81a2ed733f145a8a1332eb07f6b
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-15 05:06:59 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,53 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a != b && a != c && b != c)
 	{
-		largest = a;
+		if (a > b && a > c)
+		{
+			largest = a;
+		}
+		else if (b > a && b > c)
+		{
+			largest = b;
+		}
+		else if (c > a && c > b)
+		{
+			largest = c;
+		}
 	}
-	else if (b > a && b > c)
+	else if (a == b)
 	{
-		largest = b;
+		if (a > c)
+		{
+			largest = a;
+		}
+		else
+		{
+			largest = c;
+		}
 	}
-	else if (c > a && c > b)
+	else if (a == c)
 	{
-		largest = c;
+		if (a > b)
+		{
+			largest = a;
+		}
+		else
+		{
+			largest = b;
+		}
 	}
-
+	else if (b == c)
+	{
+		if (b > a)
+		{
+			largest = b;
+		}
+		else
+		{
+			largest = a;
+		}
+	}
 	return (largest);
 }
