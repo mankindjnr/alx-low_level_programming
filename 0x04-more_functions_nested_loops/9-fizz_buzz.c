83e689a724b7b0359bb8c722e267cbe35428faa6
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-15 03:45:11 -0700

    99.5% of programming candidates

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ *main - entry point
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%i ", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
