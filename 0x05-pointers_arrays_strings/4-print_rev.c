74ca1582a6a5090d9db6798013ba77c346fce336
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-19 08:31:04 -0700

    reversing string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+/**
+ *print_rev - reverse that string
+ */
+void print_rev(char *s)
+{
+	int count = 1, i;
+	int length = strlen(s);
+
+	for (i = 1; i <= length; i++)
+	{
+		_putchar(s[length - count]);
+		count++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+void print_rev(char *s);
+void _puts(char *str);
 int _strlen(char *s);
 int _putchar(char c);
 void reset_to_98(int *n);
