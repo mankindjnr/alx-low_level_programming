854e9161b3839291ecf80431f94c3644ae941686
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-20 03:57:49 -0700

    convert a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *_atoi - convert strings to ints
+ *@s: the string to convert
+ *Return: Always 0
+ */
+int _atoi(char *s)
+{
+	int converted;
+
+	converted = atoi(s);
+
+	return (converted);
+}
