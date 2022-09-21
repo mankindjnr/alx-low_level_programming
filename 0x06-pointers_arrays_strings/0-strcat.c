f3727ebe161b91fd1e085f17b3d274c3fc3f96a4
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-20 22:59:45 -0700

    a string that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ **_strcat - concatenation
+ *@dest: destination
+ *@src: source
+ *Return: Always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	dest = strcat(dest, src);
+
+	return (dest);
+}
