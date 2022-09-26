a1dba95afec74606ade1ed46ca4857400d339480
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-26 04:52:59 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
  *_strpbrk - finds the first character in the second string matching a in str1
  *@s: the string to scan
  *@accept: string with the character to match
- *Returns: a pointer to the character in str1 that matches a characterin str2
+ *Return: a pointer to the character in str1 that matches a characterin str2
  */
 char *_strpbrk(char *s, char *accept)
 {
