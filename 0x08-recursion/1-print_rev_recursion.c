f6cbbcab4230bbe8be1914d84e1bfb1b3673726b
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-28 03:31:11 -0700

    reversing a string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_print_rev_recursion - reverse the string
+ *@s: the string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
