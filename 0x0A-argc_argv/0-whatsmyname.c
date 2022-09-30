1a6709b5bfcdbd29ebc4afcd64e4f487f44a1239
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-30 07:14:10 -0700

    printing the first element of a command line argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ *main - entry point
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
