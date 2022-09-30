1644c2bdbd8cff913e02437810cc47d2d80283f7
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-30 08:05:37 -0700

    print argumnets one after another

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *main - entry point
+ *@argc: count the argumnets
+ *@argv: the arguments
+ *Return: AAlways 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
