a65d9447e852d0e7fdc75ecdb67e221d7404610f
Amos <kamos8646@stu.kemu.ac.ke>
2022-09-30 07:49:05 -0700

    counting number of arguments in commandline

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *main - entry point
+ *@argc: argument count
+ *@argv: arguments passed
+ *Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
