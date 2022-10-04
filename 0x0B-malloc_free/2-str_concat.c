0249fd754a05a04cc6509298a7e2628749fa86a2
Amos <kamos8646@stu.kemu.ac.ke>
2022-10-04 06:44:07 -0700

    concatenate strings and malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,12 @@ char *str_concat(char *s1, char *s2)
 	int n, j, i;
 	char *str;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	n = strlen(s1) + strlen(s2) + 1;
 
 	str = (char *)malloc(n * sizeof(char));
