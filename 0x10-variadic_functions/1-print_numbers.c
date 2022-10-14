e46880fb0d256afe6c5e8726d74b6edb7a628209
Amos <kamos8646@stu.kemu.ac.ke>
2022-10-14 04:30:29 -0700

    print integers in a variadic function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ *print_numbers - print numbers
+ *@separator - the comma
+ *@n: numbers of integers passed as parameters
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num;
+
+	va_list ptr;
+
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(ptr, int);
+		printf("%d", num);
+
+		if (i < n - 1)
+		{
+			if (separator != NULL)
+			{
+				printf("%s", separator);
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+	}
+	va_end(ptr);
+	printf("\n");
+}
