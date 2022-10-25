dabb756a0938c0527ff8050cbb28c757463f44eb
Amos <kamos8646@stu.kemu.ac.ke>
2022-10-25 13:26:33 -0700

    free a singly linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,10 +8,13 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while(head != NULL)
+	while (head->next != NULL)
 	{
-		temp = head;
-		head = head->next;
-		free(temp);
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
+	free(head->str);
+	free(head);
 }
