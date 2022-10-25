e91d81bd4c740cb9b148c6946f2337f9927fc0c9
Amos <kamos8646@stu.kemu.ac.ke>
2022-10-25 10:05:04 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,23 +1,29 @@
 #include "lists.h"
-
+#include <string.h>
 /**
- *add_node - add a node at the beginning of the list
+ *add_node - add a node at the beginning of the list - create it 1st
  *@head: pointer to the first node
- *@str: the string we are to duplicate
+ *@str: the string we are to duplicate and to assign to node
+ *the new content for the new node
  *Return: address of the new ellement
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	/*unsigned int len = 0;*/
 	char *tar = strdup(str);
 	list_t *newnode;
 
 	newnode = (list_t *)malloc(sizeof(list_t));
 
+	if (newnode == NULL)
+		return (NULL);
+
 	newnode->str = tar;
-	newnode->next = *head;
-	/*newnode->len = len;*/
+        newnode->next = *head;
+	newnode->len = strlen(str);
 	*head = newnode;
 
-	return (newnode);
+	if (*head == NULL)
+		return (NULL);
+	else
+		return (*head);
 }
