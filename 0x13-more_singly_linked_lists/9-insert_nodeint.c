22029c234ec2713065a8207daa33f5759017a0e8
Amos <kamos8646@stu.kemu.ac.ke>
2022-10-27 06:25:47 -0700

    repeat delete to with comments/ change if you can

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ *insert_nodeint_at_index - insertion of a node at a given position
+ *@head: pointer to the first node
+ *@idx: position of the new node
+ *@n: the new value at index(node)
+ Return: the address of the new node
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int count = 0, i = 1;
+	listint_t *newnode, *temp, *node;
+
+	/*create a new node*/
+
+	newnode = (listint_t *)malloc(sizeof(listint_t));
+
+	if (newnode == NULL)
+		return (NULL);
+
+	/*count the number of nodes*/
+	while ((*head) != NULL)
+	{
+		(*head) = (*head)->next;
+		count++;
+	}
+
+	/*if node can't be inserted*/
+	if (idx > count)
+		return (NULL);
+	else
+	{
+		temp = (*head);
+
+		while (i < idx)
+		{
+			temp = temp->next;
+			i++;
+		}
+
+		newnode->n = n;
+		newnode->next = temp->next;
+		temp->next = newnode;
+
+		node = temp->next;
+	}
+
+	if (node == NULL)
+		return (NULL);
+	else
+		return (node);
+}
