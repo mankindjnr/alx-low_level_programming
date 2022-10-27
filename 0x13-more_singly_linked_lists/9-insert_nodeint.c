e0c02ab48c1b983134894f8f82b272578ba3216a
Amos <kamos8646@stu.kemu.ac.ke>
2022-10-27 09:09:52 -0700

    repair 9-insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,46 +5,37 @@
  *@head: pointer to the first node
  *@idx: position of the new node
  *@n: the new value at index(node)
- Return: the address of the new node
+ *Return: the address of the new node
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int count = 0, i = 1;
 	listint_t *newnode, *temp, *node;
 
-	/*create a new node*/
-
 	newnode = (listint_t *)malloc(sizeof(listint_t));
 
 	if (newnode == NULL)
 		return (NULL);
 
-	/*count the number of nodes*/
 	while ((*head) != NULL)
 	{
 		(*head) = (*head)->next;
 		count++;
 	}
-
-	/*if node can't be inserted*/
 	if (idx > count)
 		return (NULL);
-	else
+
+	temp = (*head);
+
+	while (i < idx)
 	{
-		temp = (*head);
-
-		while (i < idx)
-		{
-			temp = temp->next;
-			i++;
-		}
-
-		newnode->n = n;
-		newnode->next = temp->next;
-		temp->next = newnode;
-
-		node = temp->next;
+		temp = temp->next;
+		i++;
 	}
+	newnode->n = n;
+	newnode->next = temp->next;
+	temp->next = newnode;
+	node = temp->next;
 
 	if (node == NULL)
 		return (NULL);
