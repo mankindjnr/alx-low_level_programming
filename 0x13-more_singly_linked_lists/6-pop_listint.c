ecf818d5e5d76c67457cfd108d6e2f2e7fc22e6d
Amos <kamos8646@stu.kemu.ac.ke>
2022-10-27 02:45:45 -0700

    delete head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,18 +3,21 @@
 /**
  *pop_listint - delete the head node
  *@head: pointer to the first node - its address
- *Return: the new head value
+ *Return: the from the previous head value
  */
 int pop_listint(listint_t **head)
 {
+	int n;
+
 	listint_t *temp;
 
-	if (head == NULL)
+	if ((*head) == NULL)
 		return (0);
 
+	n = (*head)->n;
 	temp = (*head);
 	*head = (*head)->next;
 	free(temp);
 
-	return ((*head)->n);
+	return (n);
 }
