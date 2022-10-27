63986db1328882e585b664b3443cd227e5d39599
Amos <kamos8646@stu.kemu.ac.ke>
2022-10-27 03:39:38 -0700

    accessing the nth node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ *get_nodeint_at_index - access the nth node
+ *@head: pointer to the first node - its address
+ *@index: the position of node to access
+ *Return: the accesses node(nth)
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node;
+
+	unsigned int count = 0;
+
+	while (head != NULL)
+	{
+		if (count == index)
+		{
+			node = head;
+		}
+		else
+		{
+			head = head->next;
+		}
+		count++;
+	}
+
+	if (index > count)
+		return (NULL);
+	else
+		return (node);
+
+}
