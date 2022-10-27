9b2a545cfe832c47597711c8e02ef539018d9a6d
Amos <kamos8646@stu.kemu.ac.ke>
2022-10-27 03:45:06 -0700

    accessing the nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,6 +12,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	unsigned int count = 0;
 
+	if (head == NULL)
+		return (NULL);
+
 	while (head != NULL)
 	{
 		if (count == index)
