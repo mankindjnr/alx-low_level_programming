6b09bd20b71b5fe5828ef5cd881abdd2753d161c
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-11-02 07:35:44 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,11 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
 
 	fd = creat(filename, S_IRUSR | S_IWUSR);
 
