681b304bf45fcf720038e938c946bac8bfe8ce78
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-11-02 08:50:51 -0700

    create file

--- 0x15-file_io/1-create_file_copy.c
+++ 0x15-file_io/1-create_file_copy.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd;
 
 	if (filename == NULL)
 		return (-1);
@@ -17,28 +17,14 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-
-
-	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
-
-	if (fd < 0)
+	if ((fd = creat(filename, S_IRUSR | S_IWUSR)) < 0)
 	{
-		if (errno == EEXIST)
-		{
-			fd = open(filename, O_WRONLY | O_TRUNC);
-			if (fd == -1)
-				return (-1);
-		}
-		else
-			return (-1);
+		return (-1);
 	}
-
-	for (i = 0; text_content[i] != '\0'; i++)
+	else
 	{
-		if (write(fd, &text_content[i], 1) == -1)
-			return (-1);
+		write(fd, text_content, strlen(text_content));
+		close(fd);
 	}
-	close(fd);
-
 	return (1);
 }
