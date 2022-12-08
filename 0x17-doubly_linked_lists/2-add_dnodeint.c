3f18d3e12c8af617c5cef87864d7e047a88c3148
mankindjnr <kamos8646@stu.kemu.ac.ke>
2022-12-08 06:09:57 -0800

    add beg

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ *add_dnodeint - add a node at the beginning
+ *@head: the head pointer - points to the first node
+ *@n: the new element to insert
+ *Return: the address of the new element
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode;
+
+	if ((*head) == NULL)
+	{
+		return (NULL);
+	}
+
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+
+	newnode->n = n;
+	newnode->prev = 0;
+
+	(*head)->prev = newnode;
+	newnode->next = *head;
+	*head = newnode;
+
+	return (*head);
+}
