c74050eb300e0ec0008733da8a07d60c26b8e8a3
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-01-10 23:08:08 -0800

    create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
-		return NULL;
+		return (NULL);
 
 	table->size = size;
 	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
