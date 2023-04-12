69dd9fc9a8a2dbc4cdc1f613c1ef493d07ca742a
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-04-12 06:54:51 -0700

    factorial

---
+++ 0x08-recursion/3.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+int factorial(int n)
+{
+	if (n >= 1)
+	{
+		return (n * (factorial (n - 1)));
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
